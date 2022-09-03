0fbe06dd15887296a11df683e242258615c45605
SalmanAsh <102732552+SalmanAsh@users.noreply.github.com>
2022-09-03 11:40:02 +0100

    program completed

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.lang.annotation.Annotation;
 import java.text.NumberFormat;
 import java.util.Scanner;
 
@@ -5,20 +6,45 @@ public class Main {
     public static void main(String[] args) {
         int monthInYear = 12;
         int percent = 100;
+        int amount = 0;
+        float annualRate;
+        float monthlyRate;
+        int years;
+        int numberOfPayments;
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("Amount to loan: ");
-        int amount = scan.nextInt();
+        while (true) {
+            System.out.print("Amount to loan (1k - 10M): ");
+            amount = scan.nextInt();
+            if (amount >= 1000 && amount <= 10000000) {
+                break;
+            } else {
+                System.out.println("Please enter an amount between 1,000 and 10,000,000");
+            }
+        }
 
-        System.out.print("Annual Interest Rate: ");
-        float annualRate = scan.nextFloat();
-        float monthlyRate = annualRate / percent / monthInYear;
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            annualRate = scan.nextFloat();
+            if (annualRate >= 1 && annualRate <= 15) {
+                monthlyRate = annualRate / percent / monthInYear;
+                break;
+            } else {
+                System.out.println("Please enter a valid interest rate.");
+            }
+        }
 
-        System.out.print("Period (years): ");
-        int years = scan.nextInt();
-        int numberOfPayments = years * monthInYear;
-
+        while (true) {
+            System.out.print("Period (years): ");
+            years = scan.nextInt();
+            if (years >= 1 && years <= 30) {
+                numberOfPayments = years * monthInYear;
+                break;
+            } else {
+                System.out.println("Please enter an number between 1 and 30.");
+            }
+        }
         double mortgage = amount * (monthlyRate * Math.pow(1 + monthlyRate, numberOfPayments))
                 / (Math.pow(1 + monthlyRate, numberOfPayments) - 1);
 
